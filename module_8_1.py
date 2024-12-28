a8598177d69969dc91edaaf7f4f233044820139f
dinar-khanov <hanov.di@gmail.com>
2024-12-28 12:47:18 +0500

    Add files via upload

---
+++ module_8_1.py
@@ -0,0 +1,10 @@
+def add_everything_up(a, b):
+    try:
+        return round (a + b, 3)
+    except TypeError:
+        return str(a) + str(b)
+
+# Примеры использования
+print(add_everything_up(123.456, 'строка'))  # 123.456строка
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
